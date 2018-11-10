c5813c4355abbc86dd3948047e404b798fbb0cd7
DANIEL ELIAS JIMENEZ ROJAS <1084357@est.intec.edu.do>
2018-11-10 01:20:59 -0400

    Proyecto

---
+++ ProyectoF/ProyectoF/Cancion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoF
+{
+    class Cancion
+    {
+        private int ID;
+        private string Nombre;
+        private string Artista;
+        private string Album;
+        private string Genero;
+        private double Duracion;
+        private string Calidad;
+        private string Formato;
+
+        public Cancion(int a, string b, string c, string d, string e, double f, string g, string h)
+        {
+            ID = a;
+            Nombre = b;
+            Artista = c;
+            Album = d;
+            Genero = e;
+
+        }
+    }
+}
---
+++ ProyectoF/ProyectoF/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dec;
+
+            Console.WriteLine("1- Agregar cancion");
+            Console.WriteLine("2- Listar todas las canciones");
+            Console.WriteLine("3- Editar cancion");
+            Console.WriteLine("4- Borrar cancion");
+            Console.WriteLine("5- Buscar");
+
+            dec = int.Parse(Console.ReadLine());
+
+            switch (dec)
+            {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    Buscar();
+
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Buscar()
+        {
+            int db;
+
+            Console.Clear();
+            Console.WriteLine("Buscar:\n");
+            Console.WriteLine("\t1- Por Artista");
+            Console.WriteLine("\t2- Por Genero");
+            Console.WriteLine("\t3- Por Nombre");
+
+            db = int.Parse(Console.ReadLine());
+        }
+
+        static void AgregarCancion()
+        {
+
+        }
+    }
+}
