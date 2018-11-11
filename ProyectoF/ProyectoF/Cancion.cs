a7d76ba5a07d74440a4d75623786c0467ccb37a6
Elvis Gilbert Batista Pimentel <GilbertBatista.k@gmail.com>
2018-11-11 00:13:46 -0800

    0.4
    Se separaron algunos console.writeline y se agrego el random

--- ProyectoF/ProyectoF/Cancion.cs
+++ ProyectoF/ProyectoF/Cancion.cs
@@ -26,5 +26,5 @@ namespace ProyectoF
             Genero = e;
             Duracion = f;
         }
-    }
+    }   
 }
--- ProyectoF/ProyectoF/Program.cs
+++ ProyectoF/ProyectoF/Program.cs
@@ -4,48 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ProyectoF
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Random rad = new Random(); //Objeto random
             IList<Cancion> Canciones = new List<Cancion>(); // Aqui se almacenan las canciones
-            int IdCanciones = 0; //De aqui saldra el ID de las canciones, cada vez que añadamos una cancion le sumaremos +1 a este contador.
+            int IdCanciones = rad.Next(100000, 999999); //De aqui saldra el ID de las canciones, cada vez que añadamos una cancion le sumaremos +1 a este contador. Modificacion: Al inicial el programa, se crea un numero aleatorio entre el 100000 y el 999999 y a partir de ese numero se le van asignando los ID a las cnaciones.
             bool salir = false;
             string nombre, artista, album, genero;
             double duracion;
             while (true)
             {
                 int dec;
-
                 Console.WriteLine("1- Agregar cancion");
                 Console.WriteLine("2- Listar todas las canciones");
                 Console.WriteLine("3- Editar cancion");
                 Console.WriteLine("4- Borrar cancion");
                 Console.WriteLine("5- Buscar");
                 Console.WriteLine("6- Salir");
+                Console.WriteLine(" ");
                 dec = int.Parse(Console.ReadLine());
 
                 switch (dec)
                 {
                     case 1:
-                        Console.WriteLine("Introdusca el nombre");
+                        Console.WriteLine("Introduzca el nombre");
                         nombre = Convert.ToString(Console.ReadLine());
-                        Console.WriteLine("Introdusca el artista");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Introduzca el artista");
                         artista = Convert.ToString(Console.ReadLine());
-                        Console.WriteLine("Introdusca el album");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Introduzca el album");
                         album = Convert.ToString(Console.ReadLine());
-                        Console.WriteLine("Introdusca el genero");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Introduzca el genero");
                         genero = Convert.ToString(Console.ReadLine());
-                        Console.WriteLine("Introdusca");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Introduzca la duracion");
                         duracion = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(" ");
                         Canciones.Add(new Cancion(IdCanciones, nombre, artista, album, genero, duracion));
                         IdCanciones++;
+                        Console.WriteLine("Cancion agregada exitosamente");
+                        Console.WriteLine(" ");
                         break;
 
                     case 2:
-
                         break;
 
                     case 3:
